5c7d8545b7f7f23aa508fc58239da826ab8933a0
LinkunGao <gaolinkun123@gmail.com>
2022-10-18 10:08:59 +0800

    ...

--- src/App.vue
+++ src/App.vue
@@ -64,7 +64,7 @@ let allSlices: Array<any> = [];
 
 onMounted(() => {
   console.log(
-    "%cNRRD Segmentation App %cBeta:v2.1.5",
+    "%cNRRD Segmentation App %cBeta:v2.1.6",
     "padding: 3px;color:white; background:#d94607",
     "padding: 3px;color:white; background:#219EBC"
   );
