5633d8c86f718f09e56ce776a4da0f648fa8358b
LinkunGao <gaolinkun123@gmail.com>
2023-02-08 17:45:05 +1300

    update the transfer function

--- src/App.vue
+++ src/App.vue
@@ -79,7 +79,7 @@ type selecedType = {
 
 onMounted(() => {
   console.log(
-    "%cNRRD Segmentation App %cBeta:v2.2.0",
+    "%cNRRD Segmentation App %cBeta:v2.2.5",
     "padding: 3px;color:white; background:#d94607",
     "padding: 3px;color:white; background:#219EBC"
   );
